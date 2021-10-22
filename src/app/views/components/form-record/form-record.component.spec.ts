d6c30050924ef35dfad2830a02ef615407c992b3
dev-jv <dev.javierleon@gmail.com>
2021-10-21 19:55:03 -0500

    two

---
+++ src/app/views/components/form-record/form-record.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FormRecordComponent } from './form-record.component';
+
+describe('FormRecordComponent', () => {
+  let component: FormRecordComponent;
+  let fixture: ComponentFixture<FormRecordComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FormRecordComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormRecordComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/views/components/form-record/form-record.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-record',
+  templateUrl: './form-record.component.html',
+  styleUrls: ['./form-record.component.css']
+})
+export class FormRecordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
